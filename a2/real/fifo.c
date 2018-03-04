c66b01e0336fb32fd88ffcef0f79ebef110c9862
jinming-zhang <jinming.zhang47@gmail.com>
2018-03-04 16:29:35 -0500

    d

--- a2/real/fifo.c
+++ a2/real/fifo.c
@@ -12,13 +12,23 @@ extern int debug;
 
 extern struct frame *coremap;
 
+int rank = 0;
 /* Page to evict is chosen using the fifo algorithm.
  * Returns the page frame number (which is also the index in the coremap)
  * for the page that is to be evicted.
  */
 int fifo_evict() {
-	
-	return 0;
+	// go through physical frames and find the frame with lowest rank
+	int lowest_ranked_frame;
+	int lowest_rank = memsize + 1;
+	int i;
+	for(i = 0; i < memsize; i++){
+		if(coremap[i].rank < lowest_rank){
+			lowest_rank = coremap[i].rank;
+			lowest_ranked_frame = i;
+		}
+	}
+	return lowest_ranked_frame;
 }
 
 /* This function is called on each access to a page to update any information
@@ -26,7 +36,8 @@ int fifo_evict() {
  * Input: The page table entry for the page that is being accessed.
  */
 void fifo_ref(pgtbl_entry_t *p) {
-
+    coremap[p >> PAGE_SHIFT].rank = rank;
+    rank++;
 	return;
 }
 
@@ -34,4 +45,8 @@ void fifo_ref(pgtbl_entry_t *p) {
  * replacement algorithm 
  */
 void fifo_init() {
+	int i;
+	for (i = 0; i < memsize; i++){
+		coremap[i].rank = memsize + 1;
+	}
 }
